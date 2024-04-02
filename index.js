dbc6979f2c4d2cd0a16496e9c73ec5e71131926a
Casey Gustaveson <gustcase@isu.edu>
2024-04-01 20:10:17 -0500

    prettier

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const userInputString = prompt("Enter a list of foryo flavors");
-const stringArray=userInputString.split(",");
+const userInput = prompt("Enter a list of foryo flavors");
+const stringArray=userInput.split(",");
 
 
 const myObj = {};
 
 for (let i = 0; i < stringArray.length; i++) {
-  let currentLetter = stringArray[i].trim();
-  if (currentLetter in myObj) {
-    myObj[currentLetter] += 1;
+  let currentFlavor = stringArray[i];
+  if (currentFlavor in myObj) {
+    myObj[currentFlavor] += 1;
   } else {
-    myObj[currentLetter] = 1;
+    myObj[currentFlavor] = 1;
   }
 };
 
